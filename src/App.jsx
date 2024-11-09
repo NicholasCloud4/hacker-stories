e8fa44dfa42b31be4285122efd091b9e56165855
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-11-09 09:48:02 -0500

    React Controlled Components

--- src/App.jsx
+++ src/App.jsx
@@ -43,88 +43,60 @@ const App = () => {
             objectID: 4,
         }
     ];
-    console.log("APP RENDERS");
+
+    const [searchTerm, setSearchTerm] = React.useState('React');
+
+    const handleSearch = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    const searchedStories = stories.filter((story) => {
+        return story.title.toLowerCase().includes(searchTerm.toLowerCase());
+    })
 
     return (
         <div>
             <h1>My Hacker Stories</h1>
 
-            <Search />
+            <Search onSearch={handleSearch} search={searchTerm} />
 
             <hr />
 
-            <List list={stories} />
-
-            {/* <hr />
-            <List /> */}
-
+            <List list={searchedStories} />
         </div>
     );
-}
+};
 
-const Search = () => {
-    console.log("SEARCH RENDERS");
-    const [searchTerm, setSearchTerm] = React.useState('');
-
-
-    function handleChange(event) {
-
-        // synthetic event
-        console.log(event);
-        //value of target (input of the HTML element)
-        console.log(event.target.value);
-
-        setSearchTerm(event.target.value);
-
-
-    }
-
-    function handleFocus(event) {
-        console.log(event.target.style.background = 'lightgrey')
-    }
-
-    function handleBlur(event) {
-        // synthetic event
-        console.log(event);
-        //value of target (input of the HTML element)
-        console.log(event.target.style.background = '');
-    }
-
-
+const Search = (props) => {
     return (
-        <>
-            <label htmlFor="search">Search:</label>
-            <input id="search" type="text" onChange={handleChange} onFocus={handleFocus} onBlur={handleBlur}></input>
+        <div>
+            <label htmlFor="search">Search: </label>
+            <input id="search" type="text" value={props.search} onChange={props.onSearch} />
 
-            <p>{searchTerm}</p>
+            <p>
+                Searching for <strong>{props.search}</strong>.
+            </p>
+        </div>
+    );
+};
 
-        </>
-    )
+const List = (props) => (
+    <ul>
+        {props.list.map((item) => (
+            <Item key={item.objectID} item={item} />
+        ))}
+    </ul>
+);
 
-}
-
-const List = (props) => {
-    console.log("LIST RENDERS");
-    return (
-        <ul>
-            {props.list.map((item) => {
-                return <Item key={item.objectID} item={item} />
-            })}
-        </ul>
-    )
-
-}
-
-function Item(props) {
-    console.log("ITEM RENDERS");
-    return (
-        <li key={props.item.objectID}>
-            <span><a href={props.item.url}>{props.item.title}</a></span>
-            <span>{props.item.author}</span>
-            <span>{props.item.num_comments}</span>
-            <span>{props.item.points}</span>
-        </li>
-    )
-}
+const Item = (props) => (
+    <li>
+        <span>
+            <a href={props.item.url}>{props.item.title}</a>
+        </span>
+        <span>{props.item.author}</span>
+        <span>{props.item.num_comments}</span>
+        <span>{props.item.points}</span>
+    </li>
+);
 
 export default App;
