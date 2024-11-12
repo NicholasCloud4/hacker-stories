a51a394dea822d97d77d484a7de1ad7788c50cf7
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-11-12 15:54:07 -0500

    Learned about React Asynchronous Data

--- src/App.jsx
+++ src/App.jsx
@@ -43,6 +43,14 @@ const initialStories = [
     }
 ];
 
+const getAsyncStories = () =>
+    new Promise((resolve) =>
+        setTimeout(
+            () => resolve({ data: { stories: initialStories } }),
+            2000
+        )
+    );
+
 const useStorageState = (key, initialState) => {
     const [value, setValue] = React.useState(
         localStorage.getItem(key) || initialState
@@ -62,7 +70,13 @@ const App = () => {
         'React'
     );
 
-    const [stories, setStories] = React.useState(initialStories);
+    const [stories, setStories] = React.useState([]);
+
+    React.useEffect(() => {
+        getAsyncStories().then(result => {
+            setStories(result.data.stories);
+        })
+    }, []);
 
     const handleRemoveStory = (item) => {
         const newStories = stories.filter(
